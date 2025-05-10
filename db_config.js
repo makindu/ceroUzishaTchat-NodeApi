c67165703e237c9efb1ef5ddccdbeb272ea62d09
makindu <72305144+makindu@users.noreply.github.com>
2025-05-10 09:34:50 +0200

    db

--- db_config.js
+++ db_config.js
@@ -1,18 +1,10 @@
 const DBConfig = {
-  host: "127.0.0.1",
+  host: "bfqdnq3wtsbfpfpgpz8e-mysql.services.clever-cloud.com",
   port: 3306,
-  database_name: "db_clone",
-  database_username: "root",
-  database_password: "",
+  database_name: "bfqdnq3wtsbfpfpgpz8e",
+  database_username: "ux64e9f2wneaessp",
+  database_password: "PKe5EWLo40kpNKs15uOw",
   dialect: "mysql",
 };
-// const DBConfig = {
-//   host: "bfqdnq3wtsbfpfpgpz8e-mysql.services.clever-cloud.com",
-//   port: 3306,
-//   database_name: "bfqdnq3wtsbfpfpgpz8e",
-//   database_username: "ux64e9f2wneaessp",
-//   database_password: "PKe5EWLo40kpNKs15uOw",
-//   dialect: "mysql",
-// };
 
 module.exports = DBConfig;
